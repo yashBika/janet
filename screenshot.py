88f86c2ec4cebf38c42b197562408202b8749928
sharm588 <47230612+sharm588@users.noreply.github.com>
2019-04-11 14:40:03 -0500

    Add files via upload

---
+++ screenshot.py
@@ -0,0 +1,11 @@
+import cv2
+video_capture = cv2.VideoCapture(0)
+# Check success
+if not video_capture.isOpened():
+    raise Exception("Could not open video device")
+# Read picture. ret === True on success
+ret, frame = video_capture.read()
+img_name = "NNImage.png"
+cv2.imwrite(img_name, frame)
+# Close device
+video_capture.release()
